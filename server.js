1d65390529aa4c0f24fe4a18ff29ef98a4290a11
Lilli <60227125+lillime0@users.noreply.github.com>
2023-04-01 12:49:36 +0300

    Update server.js

--- server.js
+++ server.js
@@ -28,13 +28,13 @@ mongoose.set("strictQuery", true);
 // app.use(express.urlencoded({ extended: true }));
 
 // only when ready to deploy
-// const __dirname = dirname(fileURLToPath(import.meta.url));
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.resolve(__dirname, "./client/dist")));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
-//   });
-// }
+const __dirname = dirname(fileURLToPath(import.meta.url));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "./client/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
+  });
+}
 
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messagesRouter);
